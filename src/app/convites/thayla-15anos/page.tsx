5e8e0fa4c3039eadeace5340f3f2651f2ceb37f9
David de Aguiar Canavez <david.canavez@unimeduberaba.local>
2025-08-05 03:38:16 -0300

    primeiro commit

---
+++ src/app/convites/thayla-15anos/page.tsx
@@ -0,0 +1,13 @@
+import Convite from './Convite';
+
+export const metadata = {
+  title: 'Thayla - XV Anos',
+  description: 'Um convite especial para celebrar os 15 anos da Thayla ✨',
+  icons: {
+    icon: '/convites/thayla-15anos/favicon.ico',
+  },
+};
+
+export default function Page() {
+  return <Convite />;
+}
